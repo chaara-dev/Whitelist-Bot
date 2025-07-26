7a1b1dc75f60512da88e7ef9eddf3a17f4653240
chaara-dev <github@chaara.dev>
2025-07-25 23:34:53 -0700

    Move ApplicationView out of Cog and fix button not working

--- extensions/core_function.py
+++ extensions/core_function.py
@@ -7,53 +7,58 @@ from discord.ext import commands
 
 from storage.system import Constants as constant
 
+class ApplicationView(discord.ui.View):
+    def __init__(self, bot) -> None:
+        super().__init__(timeout=None)
+        self.bot = bot
+        self.templateEmbed = discord.Embed(title="Whitelist Application Requirements", description=CoreFunction.load_whitelist_message(self), color=0x4654c0)
+
+    @discord.ui.button(label="Apply for Whitelist", style=discord.ButtonStyle.primary, custom_id="whitelist_button")
+    @commands.Cog.listener()
+    async def button_callback(self, button, interaction):
+        try:
+            channel = self.bot.get_channel(constant.APP_CHANNEL_ID)
+            if button.user.get_role(constant.MEMBER_ROLE_ID) is None or button.user.id == constant.OWNER_ID:
+                new_thread = await channel.create_thread(
+                    name=f"{button.user.name} application", 
+                    message=None, 
+                    auto_archive_duration=1440, 
+                    type=discord.ChannelType.private_thread, 
+                    reason=None,
+                    invitable=False
+                )
+
+                # time.sleep(1.5)
+                await button.response.send_message(f"Your application thread was created at <#{new_thread.id}>.",ephemeral=True)
+                await new_thread.send(embed=self.templateEmbed)
+                
+                await new_thread.send(f"**Hi <@{button.user.id}>, your whitelist application has been created. Please fill out the above application in the chat here. A staff member will review shortly.**")
+
+
+                logEmbed = discord.Embed(title="Application Created",
+                                        description=f"**User**\n<@{button.user.id}>",
+                                        color=0x4654c0,
+                                        timestamp=datetime.datetime.now()
+                                        )
+                logEmbed.add_field(name="Thread Link", value=f"<#{new_thread.id}>", inline=False)
+                logEmbed.set_thumbnail(url=f"{button.user.avatar}")
+                logEmbed.set_footer(text=f"User ID: {button.user.id}")
+                
+                logs = self.bot.get_channel(constant.LOGS_CHANNEL_ID)
+                await logs.send(embed=logEmbed)
+            
+            elif button.user.get_role(constant.MEMBER_ROLE_ID):
+                try:
+                    await button.response.send_message(f"You've already applied for the whitelist. If you have since been removed, please contact a\n Staff member.", ephemeral=True)
+                except Exception as idfk:
+                    print(f"error: {idfk}")
+                    await button.response.send_message("Something went wrong. Please try again later.\nIf the error persists, contact a Staff member.", ephemeral=True)
+        except Exception as e:
+            print(f"ERROR: {e}")
+
 class CoreFunction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.templateEmbed = discord.Embed(title="Whitelist Application Requirements", description=self.load_whitelist_message(), color=0x4654c0)
-
-
-    class ApplicationView(discord.ui.View):
-        def __init__(self) -> None:
-            super().__init__(timeout=None)
-
-        @discord.ui.button(label="Apply for Whitelist", style=discord.ButtonStyle.primary, custom_id="whitelist_button")
-        async def button_callback(self, button, interaction):
-                if not button.user.get_role(constant.MEMBER_ROLE_ID) or button.user.id == constant.OWNER_ID:
-                    channel = self.bot.get_channel(constant.APP_CHANNEL_ID)
-                    new_thread = await channel.create_thread(
-                        name=f"{button.user.name} application", 
-                        message=None, 
-                        auto_archive_duration=1440, 
-                        type=discord.ChannelType.private_thread, 
-                        reason=None,
-                        invitable=False
-                    )
-
-                    time.sleep(1.5)
-                    await button.response.send_message(f"Your application thread was created at <#{new_thread.id}>.",ephemeral=True)
-                    await new_thread.send(embed=self.templateEmbed)
-                    
-                    await new_thread.send(f"**Hi <@{button.user.id}>, your whitelist application has been created. Please fill out the above application in the chat here. A staff member will review shortly.**")
-
-
-                    logEmbed = discord.Embed(title="Application Created",
-                                            description=f"**User**\n<@{button.user.id}>",
-                                            color=0x4654c0,
-                                            timestamp=datetime.datetime.now()
-                                            )
-                    logEmbed.add_field(name="Thread Link", value=f"<#{new_thread.id}>", inline=False)
-                    logEmbed.set_thumbnail(url=f"{button.user.avatar}")
-                    logEmbed.set_footer(text=f"User ID: {button.user.id}")
-                    
-                    logs = self.bot.get_channel(constant.LOGS_CHANNEL_ID)
-                    await logs.send(embed=logEmbed)
-                
-                elif button.user.get_role(constant.MEMBER_ROLE_ID):
-                    try:
-                        await button.response.send_message(f"You've already applied for the whitelist. If you have since been removed, please contact a\n Staff member.", ephemeral=True)
-                    except:
-                        await button.response.send_message("Something went wrong. Please try again later.\nIf the error persists, contact a Staff member.", ephemeral=True)
 
 
     # load message ID from json storage for application channel
@@ -83,10 +88,11 @@ class CoreFunction(commands.Cog):
             return None
 
 
+    # check id and contents of whitelist message and update/send depending
     async def update_embed_message(self):
         last_message = None
         application_channel = self.bot.get_channel(constant.APP_CHANNEL_ID)
-        view = self.ApplicationView()
+        view = ApplicationView(self.bot)
         stored_message_id = self.load_stored_id()
         whitelist_message = self.load_whitelist_message()
 
@@ -106,6 +112,7 @@ class CoreFunction(commands.Cog):
                 return 
             else:
                 await last_message.edit(embed=self.templateEmbed, view=view)
+                print(colored("Embed message", "yellow"), "reloaded.")
         else:
             last_message = await application_channel.send(embed=self.templateEmbed, view=view)
 
@@ -114,3 +121,4 @@ class CoreFunction(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(CoreFunction(bot))
+    bot.add_view(ApplicationView(bot)) # IMPORTANT!!!
